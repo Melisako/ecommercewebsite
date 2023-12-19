5bf471b76b7a893519953cf116d3d751730505ed
Melisako <120452771+Melisako@users.noreply.github.com>
2023-12-19 01:17:05 +0100

    css-com

---
+++ src/products.js
@@ -0,0 +1,40 @@
+import product1 from "./assets/1.jpg"
+import product2 from "./assets/2.jpg"
+import product3 from "./assets/3.jpg"
+import product4 from "./assets/4.jpg"
+
+
+
+export const PRODUCTS = [
+
+{
+    id:1,
+    productName: "Iphone",
+    price: 999,
+    productImage:product1,
+
+},
+
+{
+    id:2,
+    productName: "Macbook",
+    price: 760,
+    productImage:product2,
+
+},
+{
+    id:3,
+    productName: "Samsung",
+    price: 900,
+    productImage:product3,
+
+},
+{
+    id:3,
+    productName: "Lenovo",
+    price: 1000,
+    productImage:product4,
+
+},
+
+]
