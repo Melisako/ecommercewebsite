276db3baf7df4408ec8ac2690d39f93c09a879fe
Melisako <120452771+Melisako@users.noreply.github.com>
2024-01-07 19:43:45 +0100

    css-commit

--- src/pages/aboutus/aboutus.jsx
+++ src/pages/aboutus/aboutus.jsx
@@ -1,15 +1,27 @@
 // AboutUs.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import './about.css';
 import CompanyImage from '../../assets/company-image.jpg';
 
 const AboutUs = () => {
+const[isOpen,setIsOpen]=useState(true)
+const[image,setImage]=useState(false)
+const showImg=()=>{
+  setImage(!image)
+}
+const toggleButton=()=>{
+  setIsOpen(!isOpen)
+}
+
   return (
     <div className="about-container">
       <div className="about-content">
         <h2>About Our Company</h2>
-        <p>
+     
+          {isOpen&&(
+            <>
+               <p>
           Welcome to our skincare journey! At Skincare Shop, we are passionate about delivering top-quality skincare products that nourish and revitalize your skin. Our mission is to provide innovative, safe, and effective solutions for all skin types.
         </p>
         <p>
@@ -18,7 +30,11 @@ const AboutUs = () => {
         <p>
           Join us in embracing healthy, radiant skin. Explore our collection and discover the perfect regimen tailored for your skincare needs.
         </p>
+        </>
+        )}
+            <button onClick={toggleButton} className='custom-button'>{isOpen?"Hide":"Show"}</button>
       </div>
+  
       <div className="image-container">
         <img src={CompanyImage} alt="Company" className="company-image" />
       </div>
